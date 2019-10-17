7de68ebdbc9c100b5a8f9cdaceb5e2da8b2b52d3
Vijayan <vishal.vijayan@kit.edu>
2019-10-17 14:42:27 +0200

    test

--- VISHAL/test_figure_tree.m
+++ VISHAL/test_figure_tree.m
@@ -30,18 +30,7 @@ clear X data trainInd testInd dates
 model = TreeBagger(20, trainX, trainY, 'method', 'regression', 'minleaf', 40)
 
 
-oobError = [];
-leafSizes = [10 20 40 50];
-for i = 1:length(leafSizes)
-    model = TreeBagger(20, trainX, trainY, 'method', 'regression', ...
-                       'oobpred', 'on', 'minleaf', leafSizes(i));
-    oobError = [oobError model.oobError];
-    
-    figure(1), plot(oobError);
-    xlabel('Number of grown trees'), ylabel('Out-of-bag Regression Error');
-    title(sprintf('Regression Error versus Number of Trees & Leaf Size'));
-    legend(num2str(leafSizes(1:i)')), drawnow;
-end
+
 
 %% Determine Feature Importance
  
@@ -49,7 +38,7 @@ end
 model = TreeBagger(20, trainX, trainY, 'method', 'regression', ...
                    'oobvarimp', 'on', 'minleaf', 30);
 
-figure(2);
+figure(1);
 barh(model.OOBPermutedVarDeltaError);
 ylabel('Feature');
 xlabel('Out-of-bag feature importance');
@@ -57,3 +46,38 @@ title('Feature importance results');
 set(gca, 'YTickLabel', labels)
 
 
+model = TreeBagger(20, trainX, trainY, 'method', 'regression', 'minleaf', 20);
+
+
+model = compact(model);
+save Models\TreeModel_aus model
+
+
+clear
+load Models\TreeModel_aus
+load Data\testSet_aus
+
+forecastLoad = predict(model, testX);
+cd Util
+figure(2);
+ax1 = subplot(2,1,1);
+plot(testDates, [testY forecastLoad]);
+ylabel('Load'); legend({'Actual', 'Forecast'}); legend('boxoff')
+ax2 = subplot(2,1,2);
+plot(testDates, testY-forecastLoad);
+xlabel('Date'); ylabel('Error (MWh)');
+linkaxes([ax1 ax2], 'x');
+dynamicDateTicks([ax1 ax2], 'linked')
+
+
+
+err = testY-forecastLoad;
+errpct = abs(err)./testY*100;
+% fL = reshape(forecastLoad, 24, length(forecastLoad)/24)';
+% tY = reshape(testY, 24, length(testY)/24)';
+% peakerrpct = abs(max(tY,[],2) - max(fL,[],2))./max(tY,[],2) * 100;
+fL = reshape(forecastLoad(1:end-1), 1, (length(forecastLoad)-1)/1)';
+tY = reshape(testY(1:end-1), 1, (length(testY)-1)/1)';
+peakerrpct = abs(max(tY,[],2) - max(fL,[],2))./max(tY,[],2) * 100;
+fprintf('Mean Average Percent Error (MAPE): %0.2f%% \nMean Average Error (MAE): %0.2f MWh\nDaily Peak MAPE: %0.2f%%\n',...
+    mean(errpct(~isinf(errpct))), mean(abs(err)), mean(peakerrpct))
